1ce8ada4dea13cd96b2e57e241d7f240094ee283
Anssi Mäkinen <amakinen@student.hive.fi>
2025-02-25 22:07:06 +0200

    Test redirections for simple_command and pipeline

--- src/test/pipeline.c
+++ src/test/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 18:35:02 by amakinen          #+#    #+#             */
-/*   Updated: 2025/02/25 19:36:07 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/02/25 22:01:15 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,28 @@
 #include "execute.h"
 
 /*
+	/bin/echo -e first\n\n\ntest > test_pipeline_tmpout |
 	/bin/echo -e hello\n world! |
 	/bin/grep world |
-	/bin/wc -c
+	/bin/wc -c > /dev/stderr |
+	/bin/wc < test_pipeline_tmpout
 */
 struct s_ast_simple_command	*g_test_pipeline
 	= &(struct s_ast_simple_command){
+	.args = &(struct s_ast_command_word){
+	.word = "/bin/echo",
+	.next = &(struct s_ast_command_word){
+	.word = "-e",
+	.next = &(struct s_ast_command_word){
+	.word = "first\\n\\n\\ntest",
+	.next = NULL,
+},},},
+	.redirs = &(struct s_ast_redirect){
+	.op = AST_REDIR_OUT,
+	.word = "test_pipeline_tmpout",
+	.next = NULL,
+},
+	.next = &(struct s_ast_simple_command){
 	.args = &(struct s_ast_command_word){
 	.word = "/bin/echo",
 	.next = &(struct s_ast_command_word){
@@ -48,9 +64,23 @@ struct s_ast_simple_command	*g_test_pipeline
 	.word = "-c",
 	.next = NULL,
 },},
-	.redirs = NULL,
-	.next = NULL
-},},};
+	.redirs = &(struct s_ast_redirect){
+	.op = AST_REDIR_OUT,
+	.word = "/dev/stderr",
+	.next = NULL,
+},
+	.next = &(struct s_ast_simple_command){
+	.args = &(struct s_ast_command_word){
+	.word = "/bin/wc",
+	.next = NULL,
+},
+	.redirs = &(struct s_ast_redirect){
+	.op = AST_REDIR_IN,
+	.word = "test_pipeline_tmpout",
+	.next = NULL,
+},
+	.next = NULL,
+},},},},};
 
 int	main(void)
 {
--- src/test/simple_command.c
+++ src/test/simple_command.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 18:11:56 by amakinen          #+#    #+#             */
-/*   Updated: 2025/02/25 19:08:15 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/02/25 22:00:53 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "execute.h"
 
 /*
-	/bin/echo hello world!
+	/bin/echo hello world! > test_simple_trunc_nooutput >> test_simple_app
 */
 struct s_ast_simple_command	*g_test_command
 	= &(struct s_ast_simple_command){
@@ -28,7 +28,14 @@ struct s_ast_simple_command	*g_test_command
 	.word = "world!",
 	.next = NULL,
 },},},
-	.redirs = NULL,
+	.redirs = &(struct s_ast_redirect){
+	.op = AST_REDIR_OUT,
+	.word = "test_simple_trunc_nooutput",
+	.next = &(struct s_ast_redirect){
+	.op = AST_REDIR_APP,
+	.word = "test_simple_app",
+	.next = NULL,
+},},
 	.next = NULL,
 };
 

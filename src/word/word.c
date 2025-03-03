4065655a7cc21501046b83720098d95eced4e633
Anssi Mäkinen <amakinen@student.hive.fi>
2025-03-03 22:17:32 +0200

    Create empty stub for word_expand

---
+++ src/word/word.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   word.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 16:54:57 by amakinen          #+#    #+#             */
+/*   Updated: 2025/02/28 17:15:31 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "word.h"
+
+#include <stddef.h>
+
+/*
+	Perform all word expansions on the given word and return the resulting field
+	list.
+
+	TODO: implement
+*/
+struct s_word_field	*word_expand(char *word)
+{
+	(void)word;
+	return (NULL);
+}

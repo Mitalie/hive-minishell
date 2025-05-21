66a1c2aa43b3f03eb2f37cdbb9a9de2575a3db6f
Anssi Mäkinen <amakinen@student.hive.fi>
2025-05-21 07:12:50 +0300

    execute: Refactor execute_pipeline, add error handling

--- src/execute/pipeline.c
+++ src/execute/pipeline.c
@@ -6,13 +6,14 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:13:08 by amakinen          #+#    #+#             */
-/*   Updated: 2025/05/21 03:54:04 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/05/29 21:36:25 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute_internal.h"
 #include "execute.h"
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -25,75 +26,121 @@
 
 /*
 	Create a pipe if necessary and determine input/output FDs for the current
-	command.
-	TODO: handle pipe() error
+	command, then fork a child process and store the result in fork_result.
 */
-static void	pipeline_create_pipe(struct s_pipeline_fds *fds,
-	bool first, bool last)
+static t_status	execute_pipeline_create_pipe_and_fork(
+	struct s_pipeline_fds *fds, bool first, bool last, pid_t *fork_result)
 {
 	int	pipe_fd[2];
 
-	if (first)
-		fds->in = NO_PIPE;
-	else
+	fds->in = NO_PIPE;
+	if (!first)
 		fds->in = fds->next_in;
-	if (last)
+	fds->next_in = NO_PIPE;
+	fds->out = NO_PIPE;
+	if (!last)
 	{
-		fds->next_in = NO_PIPE;
-		fds->out = NO_PIPE;
-	}
-	else
-	{
-		pipe(pipe_fd);
+		if (pipe(pipe_fd) < 0)
+			return (status_err(S_RESET_ERR, "execute_pipeline",
+					"pipe() failed", errno));
 		fds->next_in = pipe_fd[0];
 		fds->out = pipe_fd[1];
 	}
+	*fork_result = fork();
+	if (*fork_result < 0)
+		return (status_err(S_RESET_ERR, "execute_pipeline",
+				"fork() failed", errno));
+	return (S_OK);
 }
 
 /*
-	Move the input and output pipes to correct FD numbers, and close the read
-	end of the pipe as it belongs to the next command and not this one.
-	TODO: handle dup2() error
-	TODO: report close() error
+	Move the input and output pipes to correct FD numbers, close the read end of
+	the output pipe, and execute the command. Set the status to exit from the
+	child process if the command didn't execve.
+
+	We don't need to check for close() errors here. They are only useful for
+	reporting potential data loss in case of a delayed write error, or reporting
+	that a delayed write may still be pending in case of EINTR. Such delayed
+	writes that are flushed on close don't apply to pipes, and we haven't
+	written anything yet anyway.
 */
-static void	pipeline_cleanup_child(struct s_pipeline_fds *fds)
+static t_status	execute_pipeline_child(struct s_pipeline_fds *fds,
+	struct s_ast_simple_command *child_command, t_env *env, int *exit_code)
 {
+	t_status	status;
+
+	status = S_OK;
 	if (fds->in != NO_PIPE)
 	{
-		dup2(fds->in, STDIN_FILENO);
+		if (dup2(fds->in, STDIN_FILENO) < 0)
+			status = status_err(S_EXIT_ERR, "execute_pipeline_child",
+					"dup2() failed", errno);
 		close(fds->in);
 	}
 	if (fds->next_in != NO_PIPE)
 		close(fds->next_in);
 	if (fds->out != NO_PIPE)
 	{
-		dup2(fds->out, STDOUT_FILENO);
+		if (dup2(fds->out, STDOUT_FILENO) < 0)
+			status = status_err(S_EXIT_ERR, "execute_pipeline_child",
+					"dup2() failed", errno);
 		close(fds->out);
 	}
+	if (status == S_OK)
+		status = execute_simple_command(child_command, env, exit_code);
+	return (status_force_exit(status, exit_code));
 }
 
 /*
 	Close the pipe FDs created for the current command and not needed for the
-	next one.
-	TODO: report close() error
+	next one. Also close read end of the next pipe if fork failed and we're not
+	continuing to next command.
+
+	We don't need to check for close() errors here. They are only useful for
+	reporting potential data loss in case of a delayed write error, or reporting
+	that a delayed write may still be pending in case of EINTR. Such delayed
+	writes that are flushed on close don't apply to pipes, and we haven't
+	written anything yet anyway.
 */
-static void	pipeline_cleanup_parent(struct s_pipeline_fds *fds)
+static void	execute_pipeline_cleanup(struct s_pipeline_fds *fds, bool had_error)
 {
 	if (fds->in != NO_PIPE)
 		close(fds->in);
+	if (had_error && fds->next_in != NO_PIPE)
+		close(fds->next_in);
 	if (fds->out != NO_PIPE)
 		close(fds->out);
 }
 
-static t_status	pipeline_wait_for_children(int num_children, pid_t last_child,
-	int	*exit_code)
+/*
+	Wait until all child processes have exited. Store the exit status of the
+	last process of the pipeline. Pass through the given t_status unless an
+	unexpected error happens.
+
+	If fork fails, last_child is -1 and exit_code remains unmodified, but in an
+	error case the returned status should override exit_code anyway.
+
+	When there are no more child processes, wait fails with ECHILD. The actual
+	error case should be impossible to hit, but no harm in checking.
+*/
+static t_status	execute_pipeline_wait_for_children(
+	t_status status, pid_t last_child, int *exit_code)
 {
 	int		wait_status;
 	pid_t	waited_child;
 
-	while (num_children--)
+	while (true)
 	{
 		waited_child = wait(&wait_status);
+		if (waited_child < 0)
+		{
+			if (errno == EINTR)
+				continue ;
+			if (errno == ECHILD)
+				return (status);
+			return (status_err(S_EXIT_ERR, "execute_pipeline: internal error",
+					"wait failed", errno));
+		}
 		if (waited_child == last_child)
 		{
 			if (WIFEXITED(wait_status))
@@ -102,41 +149,36 @@ static t_status	pipeline_wait_for_children(int num_children, pid_t last_child,
 				*exit_code = 128 + WTERMSIG(wait_status);
 		}
 	}
-	return (S_OK);
 }
 
 /*
 	Execute all commands in a pipeline in parallel, connecting the stdout of
 	each command to the stdin of the next one.
-	TODO: handle fork() error
 	TODO: handle single builtin in main process
 */
 t_status	execute_pipeline(struct s_ast_simple_command *pipeline_head,
 	t_env *env, int *exit_code)
 {
+	t_status				status;
 	pid_t					child;
 	bool					first;
 	struct s_pipeline_fds	fds;
-	int						num_children;
 
+	status = S_OK;
 	first = true;
-	num_children = 0;
 	child = 0;
 	while (pipeline_head)
 	{
-		num_children++;
-		pipeline_create_pipe(&fds, first, !pipeline_head->next);
-		child = fork();
-		if (child == 0)
-		{
-			pipeline_cleanup_child(&fds);
-			execute_simple_command(pipeline_head, env, exit_code);
-			return (S_EXIT_OK);
-		}
-		else
-			pipeline_cleanup_parent(&fds);
+		status = execute_pipeline_create_pipe_and_fork(
+				&fds, first, !pipeline_head->next, &child);
+		if (status == S_OK && child == 0)
+			return (execute_pipeline_child(&fds, pipeline_head, env,
+					exit_code));
+		execute_pipeline_cleanup(&fds, status != S_OK);
+		if (status != S_OK)
+			break ;
 		pipeline_head = pipeline_head->next;
 		first = false;
 	}
-	return (pipeline_wait_for_children(num_children, child, exit_code));
+	return (execute_pipeline_wait_for_children(status, child, exit_code));
 }

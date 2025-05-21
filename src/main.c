de00c0e6e7a56c02194b86a9b79f74191bb13b82
Anssi Mäkinen <amakinen@student.hive.fi>
2025-05-21 03:27:43 +0300

    main: Handle exit codes and terminate loop when requested

--- src/main.c
+++ src/main.c
@@ -6,10 +6,11 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:55:33 by amakinen          #+#    #+#             */
-/*   Updated: 2025/05/12 20:28:40 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/05/21 03:26:47 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/history.h>
@@ -21,35 +22,43 @@
 #include "env.h"
 #include "status.h"
 
-/*
-	TODO: exit status for errors
-	TODO: message and cleanup for malloc and other fatal errors
-*/
-
-int	main(void)
+static t_status	minishell_do_line(t_env *env, int *exit_code)
 {
 	char					*line;
 	struct s_ast_list_entry	*ast;
 	t_status				status;
-	t_env					env;
 
+	line = readline("minishell> ");
+	if (!line)
+		return (S_EXIT_OK);
+	status = parser_parse(line, &ast);
+	if (status == S_OK && ast)
+		add_history(line);
+	free(line);
+	if (status == S_OK)
+		*exit_code = execute_list(ast, env);
+	free_ast(ast);
+	if (status == S_EXIT_ERR || status == S_RESET_ERR)
+		*exit_code = 1;
+	if (status == S_RESET_SYNTAX)
+		*exit_code = 2;
+	if (status == S_RESET_SIGINT)
+		*exit_code = 128 + SIGINT;
+	return (status);
+}
+
+int	main(void)
+{
+	t_status	status;
+	t_env		env;
+	int			exit_code;
+
+	exit_code = 0;
 	status = env_init(&env);
 	if (status != S_OK)
 		return (1);
-	while (1)
-	{
-		line = readline("minishell> ");
-		if (!line)
-			break ;
-		status = parser_parse(line, &ast);
-		if (status == S_OK && ast)
-			add_history(line);
-		free(line);
-		if (status == S_OK)
-			execute_list(ast, &env);
-		free_ast(ast);
-		if (status == S_EXIT_ERR)
-			return (1);
-	}
+	while (status != S_EXIT_ERR && status != S_EXIT_OK)
+		status = minishell_do_line(&env, &exit_code);
 	env_free(&env);
+	return (exit_code);
 }

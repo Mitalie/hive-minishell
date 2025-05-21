cfea6f63a9ea74e03d5304d90f7d6bfd3f57fe1b
Anssi Mäkinen <amakinen@student.hive.fi>
2025-05-21 03:27:43 +0300

    main: Handle exit codes and terminate loop when requested

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:55:33 by amakinen          #+#    #+#             */
-/*   Updated: 2025/05/12 20:28:40 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/05/29 21:20:54 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,38 @@
 #include "env.h"
 #include "status.h"
 
-/*
-	TODO: exit status for errors
-	TODO: message and cleanup for malloc and other fatal errors
-*/
+static t_status	minishell_do_line(t_env *env, int *exit_code)
+{
+	t_status				status;
+	char					*line;
+	struct s_ast_list_entry	*ast;
+
+	line = readline("minishell> ");
+	if (!line)
+		return (S_EXIT_OK);
+	status = parser_parse(line, &ast);
+	if (status == S_OK && ast)
+		add_history(line);
+	free(line);
+	if (status == S_OK)
+		*exit_code = execute_list(ast, env);
+	free_ast(ast);
+	status_set_exit_code(status, exit_code);
+	return (status);
+}
 
 int	main(void)
 {
-	char					*line;
-	struct s_ast_list_entry	*ast;
-	t_status				status;
-	t_env					env;
+	t_status	status;
+	t_env		env;
+	int			exit_code;
 
+	exit_code = 0;
 	status = env_init(&env);
 	if (status != S_OK)
 		return (1);
-	while (1)
-	{
-		line = readline("minishell> ");
-		if (!line)
-			break ;
-		status = parser_parse(line, &ast);
-		if (status == S_OK && ast)
-			add_history(line);
-		free(line);
-		if (status == S_OK)
-			execute_list(ast, &env);
-		free_ast(ast);
-		if (status == S_EXIT_ERR)
-			return (1);
-	}
+	while (status != S_EXIT_ERR && status != S_EXIT_OK)
+		status = minishell_do_line(&env, &exit_code);
 	env_free(&env);
+	return (exit_code);
 }

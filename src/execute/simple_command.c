c187ad3ef6adad4bb196dfb0ca8681670460c389
Anssi Mäkinen <amakinen@student.hive.fi>
2025-05-26 21:16:50 +0300

    execute: Expand command arguments

--- src/execute/simple_command.c
+++ src/execute/simple_command.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 17:21:06 by amakinen          #+#    #+#             */
-/*   Updated: 2025/05/21 03:42:29 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/05/26 21:16:05 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 #include "builtin.h"
 #include "env.h"
 #include "status.h"
+#include "word.h"
 
 int	process_heredoc(struct s_ast_redirect *redirect);
 
@@ -74,15 +75,15 @@ static void	apply_redirects(struct s_ast_redirect *redirs)
 }
 
 /*
-	Converts the AST command word list to an argv array for execve
-	Allocates a new array with command and arguments, NULL-terminated
-	TODO: apply word processing steps
+	Count args in the given field list, allocate an argv-style NULL-terminated
+	pointer array, and fill it with pointers to field values.
 */
-static char	**build_argv(struct s_ast_command_word *args)
+static t_status	execute_arg_fields_to_argv(
+	struct s_word_field *args, char ***argv_out)
 {
-	size_t						n_args;
-	char						**argv;
-	struct s_ast_command_word	*current_arg;
+	size_t				n_args;
+	char				**argv;
+	struct s_word_field	*current_arg;
 
 	n_args = 0;
 	current_arg = args;
@@ -92,18 +93,51 @@ static char	**build_argv(struct s_ast_command_word *args)
 		current_arg = current_arg->next;
 	}
 	argv = malloc((n_args + 1) * sizeof(*argv));
+	*argv_out = argv;
 	if (!argv)
-		return (NULL);
+		return (status_err(S_EXIT_ERR, ERRMSG_MALLOC, NULL, 0));
 	n_args = 0;
 	current_arg = args;
 	while (current_arg)
 	{
-		argv[n_args] = current_arg->word;
+		argv[n_args] = current_arg->value;
 		n_args++;
 		current_arg = current_arg->next;
 	}
 	argv[n_args] = NULL;
-	return (argv);
+	return (S_OK);
+}
+
+/*
+	Expand all words in AST command word list into a field list, and build an
+	argv array of field values that can be passed to execve.
+
+	The argv array entries point to strings owned by the fields list, so the
+	field list must not be freed while the argv array is in use. The caller
+	is responsible for freeing the fields list, even in case of failure.
+
+	If expansion produces no fields, no memory is allocated and the out pointers
+	are set to null pointers.
+*/
+static t_status	execute_expand_args(struct s_ast_command_word *args,
+	struct s_word_field **fields_out, char ***argv_out)
+{
+	t_status			status;
+	struct s_word_field	**fields_append;
+
+	*fields_out = NULL;
+	*argv_out = NULL;
+	fields_append = fields_out;
+	while (args)
+	{
+		status = word_expand(args->word, &fields_append);
+		if (status != S_OK)
+			return (status);
+		args = args->next;
+	}
+	if (!*fields_out)
+		return (S_OK);
+	return (execute_arg_fields_to_argv(*fields_out, argv_out));
 }
 
 /*
@@ -115,22 +149,21 @@ static char	**build_argv(struct s_ast_command_word *args)
 t_status	execute_simple_command(struct s_ast_simple_command *command,
 	t_env *env, int *exit_code)
 {
-	t_status		status;
-	char			**argv;
-	t_builtin_func	*builtin;
+	t_status			status;
+	struct s_word_field	*arg_fields;
+	char				**argv;
+	t_builtin_func		*builtin;
 
 	apply_redirects(command->redirs);
-	if (!command->args)
-		return (S_OK);
-	argv = build_argv(command->args);
-	if (!argv)
-		return (S_EXIT_ERR);
+	status = execute_expand_args(command->args, &arg_fields, &argv);
+	if (status != S_OK || !argv)
+		return (status);
 	builtin = builtin_get_func(argv[0]);
-	status = S_OK;
 	if (builtin)
 		status = builtin(argv, env, exit_code, 1);
 	else
 		handle_path_search(argv, env, exit_code);
 	free(argv);
+	word_free(arg_fields);
 	return (status);
 }

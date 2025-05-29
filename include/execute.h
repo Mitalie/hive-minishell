91cca4d1abf0c0331b9fe3bb9b8b3fca3e6edb43
Anssi Mäkinen <amakinen@student.hive.fi>
2025-05-29 20:16:55 +0300

    execute: Refactor execute_simple_command to support in-process builtins

--- include/execute.h
+++ include/execute.h
@@ -6,12 +6,14 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 17:28:43 by amakinen          #+#    #+#             */
-/*   Updated: 2025/05/21 03:37:21 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/05/29 20:10:05 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTE_H
 # define EXECUTE_H
+
+# include <stdbool.h>
 
 # include "env.h"
 # include "status.h"
@@ -21,7 +23,7 @@ struct	s_ast_list_entry;
 struct	s_ast_redirect;
 
 t_status	execute_simple_command(struct s_ast_simple_command *command,
-				t_env *env, int *exit_code);
+				t_env *env, int *exit_code, bool is_child);
 t_status	execute_pipeline(struct s_ast_simple_command *pipeline_head,
 				t_env *env, int *exit_code);
 t_status	execute_list(struct s_ast_list_entry *list_head,
--- src/execute/simple_command.c
+++ src/execute/simple_command.c
@@ -6,15 +6,19 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 17:21:06 by amakinen          #+#    #+#             */
-/*   Updated: 2025/05/29 19:19:57 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/05/29 20:09:55 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 #include "execute_internal.h"
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "ast.h"
 #include "builtin.h"
@@ -89,32 +93,97 @@ static t_status	execute_expand_args(struct s_ast_command_word *args,
 }
 
 /*
-	Executes a simple command from the AST
-	Handles redirections, builds argument array, and executes the command
-	Exits with appropriate status code based on command execution result
-	TODO: handle builtins
+	Fork a child process for executing an external command. Child process
+	sets is_child and returns, while main process waits for child to exit and
+	stores its exit status in exit_code.
+*/
+static t_status	execute_command_fork(bool *is_child, int *exit_code)
+{
+	pid_t	child_pid;
+	int		wait_status;
+
+	child_pid = fork();
+	if (child_pid < 0)
+		return (status_err(S_RESET_ERR, "execute_command",
+				"fork() failed", errno));
+	if (child_pid == 0)
+	{
+		*is_child = true;
+		return (S_OK);
+	}
+	while (wait(&wait_status) < 0)
+	{
+		if (errno == EINTR)
+			continue ;
+		return (status_err(S_EXIT_ERR, "execute_command: internal error",
+				"wait() failed", errno));
+	}
+	if (WIFEXITED(wait_status))
+		*exit_code = WEXITSTATUS(wait_status);
+	else if (WIFSIGNALED(wait_status))
+		*exit_code = 128 + WTERMSIG(wait_status);
+	return (S_OK);
+}
+
+/*
+	Apply redirections and execute the command according to its type. External
+	commands move redirections to their correct file descriptor numbers, while
+	builtins and empty commands use the the temporary file descriptors directly
+	and close them afterwards.
+*/
+static t_status	execute_command_execute(struct s_ast_redirect *redirs,
+	char **argv, t_env *env, int *exit_code)
+{
+	t_status			status;
+	struct s_redir_fds	fds;
+	t_builtin_func		*builtin;
+	bool				is_external;
+
+	status = execute_redirect_prepare(&fds, redirs);
+	if (status != S_OK)
+		return (status);
+	builtin = NULL;
+	if (argv)
+		builtin = builtin_get_func(argv[0]);
+	is_external = argv && !builtin;
+	if (is_external)
+		status = execute_redirect_finish(&fds, true);
+	if (is_external && status == S_OK)
+		handle_path_search(argv, env, exit_code);
+	if (builtin && fds.out == NO_REDIR)
+		status = builtin(argv, env, exit_code, STDOUT_FILENO);
+	else if (builtin)
+		status = builtin(argv, env, exit_code, fds.out);
+	if (!is_external)
+		execute_redirect_finish(&fds, false);
+	return (status);
+}
+
+/*
+	Execute a simple command from the AST
+
+	Expands arguments, builds argv array and forks a child process if necessary.
+	If forked, parent waits for child and collects its exit code. If not forked
+	or in child, applies redirections and executes the command. Forces the child
+	process to exit instead of staying around as a duplicate shell instance.
 */
 t_status	execute_simple_command(struct s_ast_simple_command *command,
-	t_env *env, int *exit_code)
+	t_env *env, int *exit_code, bool is_child)
 {
 	t_status			status;
 	struct s_word_field	*arg_fields;
 	char				**argv;
-	t_builtin_func		*builtin;
-	struct s_redir_fds	fds;
+	bool				need_child;
 
-	status = execute_redirect_prepare(&fds, command->redirs);
+	status = execute_expand_args(command->args, &arg_fields, &argv);
 	if (status == S_OK)
-		status = execute_redirect_finish(&fds, true);
-	if (status == S_OK)
-		status = execute_expand_args(command->args, &arg_fields, &argv);
-	if (status != S_OK || !argv)
-		return (status);
-	builtin = builtin_get_func(argv[0]);
-	if (builtin)
-		status = builtin(argv, env, exit_code, 1);
-	else
-		handle_path_search(argv, env, exit_code);
+		need_child = argv && !builtin_get_func(argv[0]);
+	if (status == S_OK && need_child && !is_child)
+		status = execute_command_fork(&is_child, exit_code);
+	if (status == S_OK && (!need_child || is_child))
+		status = execute_command_execute(command->redirs, argv, env, exit_code);
+	if (is_child)
+		status = status_force_exit(status, exit_code);
 	free(argv);
 	word_free(arg_fields);
 	return (status);

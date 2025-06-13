9967838468b2ce36f3af81f75b93ff54ff95e42c
Anssi Mäkinen <amakinen@student.hive.fi>
2025-06-13 19:32:38 +0300

    Use a simplified line reader instead of readline when input is not a tty

--- include/input.h
+++ include/input.h
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 17:45:47 by amakinen          #+#    #+#             */
-/*   Updated: 2025/06/06 17:53:55 by amakinen         ###   ########.fr       */
+/*   Updated: 2025/06/13 18:50:19 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ t_status	input_get_line(char **line_out, const char *prompt);
 void		input_add_history(const char *line);
 
 void		input_clear_history(void);
+
+t_status	input_notty_get_line(char **line_out, const char *prompt);
 
 #endif
